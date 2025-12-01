3d35de6efc63a28e9c90199a46cc1d96f793d8b4
AshinaGenichiro <uT_4011@zsp3zamosc.pl>
2025-12-01 13:41:32 +0100

    zadanie 14.2

---
+++ databinding/MainPage.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Runtime.ExceptionServices;
+using System.Windows.Input;
+
+namespace databinding
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool>? _canExecute;
+
+        public event EventHandler? CanExecuteChanged;
+
+        public RelayCommand(Action execute, Func<bool>? canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter) => _canExecute == null || _canExecute();
+
+        public void Execute(object? parameter) => _execute();
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+    public class User : INotifyPropertyChanged
+    {
+        private string firstName ="przykładowe dane";
+        private string lastName = "przykładowe dane";
+        private string email = "przykładowe dane";
+        private int age = 2;
+        
+        public string FirstName
+        {
+            get => firstName;
+            set
+            {
+                if (firstName != value)
+                {
+                    firstName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public string LastName
+        {
+            get => lastName;
+            set
+            {
+                if (lastName != value)
+                {
+                    lastName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public string Email
+        {
+            get => email;
+            set
+            {
+                if (email != value)
+                {
+                    email = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public int Age
+        {
+            get => age;
+            set
+            {
+                if (age != value)
+                {
+                    age = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public User()
+        {
+            ChangeDataCommand = new RelayCommand(ChangeData);
+        }
+        public ICommand ChangeDataCommand { get; }
+        private void ChangeData()
+        {
+            FirstName = "Jan";
+            LastName = "Kowalski";
+            Email = "losowyV2@gmail.com";
+            Age = 25;
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    
+    }
+        public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new User();
+        }
+
+        private void OnCounterClicked(object? sender, EventArgs e)
+        {
+        }
+
+    }
+
+}
+

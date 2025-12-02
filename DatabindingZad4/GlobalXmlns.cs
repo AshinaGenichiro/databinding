f5018bf2dc82d628eb1e8648f1f654d502f2145c
AshinaGenichiro <uT_4011@zsp3zamosc.pl>
2025-12-02 11:47:07 +0100

    zadanie 14.3

---
+++ DatabindingZad4/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "DatabindingZad4")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "DatabindingZad4.Pages")]
